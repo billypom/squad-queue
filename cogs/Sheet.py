afb71ed8ac0ca008f7373660a8da41b688f1045d
billy pompey <billypompey@icloud.com>
2022-08-13 15:53:34 -0400

    a

--- cogs/Sheet.py
+++ cogs/Sheet.py
@@ -24,6 +24,9 @@ class Sheet(commands.Cog):
         
     #async def mmr(self, member: discord.Member):
     async def mmr(self, members):
+        print('WHAT IS THIS??')
+        print(members)
+        print(type(members))
         with DBA.DBAccess() as db:
             check_values = db.query('SELECT mmr FROM player WHERE player_name = %s;', (member,))
         # mmrs.update('B3:B%d' % int(2+len(members)), [[member] for member in members])
