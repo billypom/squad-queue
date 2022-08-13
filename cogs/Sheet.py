8ab6fae11a5d272d76025f29574cd866170d710a
billy pompey <billypompey@icloud.com>
2022-08-13 16:09:52 -0400

    a

--- cogs/Sheet.py
+++ cogs/Sheet.py
@@ -28,7 +28,8 @@ class Sheet(commands.Cog):
         for member in members:
             print(f'for member in member: {member}')
             with DBA.DBAccess() as db:
-                check_values = db.query('SELECT mmr FROM player WHERE player_name = %s;', (member,))
+                temp = db.query('SELECT mmr FROM player WHERE player_name = %s;', (member,))
+                check_values.append(temp[0][0])
         # mmrs.update('B3:B%d' % int(2+len(members)), [[member] for member in members])
         # check_values = mmrs.get('C3:C%d' % int(2+len(members)))
         return_mmrs = []
