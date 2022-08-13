98aa010e905654336f23cad972026d63a9db72e2
billy pompey <billypompey@icloud.com>
2022-08-13 16:04:03 -0400

    a

--- cogs/Sheet.py
+++ cogs/Sheet.py
@@ -24,12 +24,15 @@ class Sheet(commands.Cog):
         
     #async def mmr(self, member: discord.Member):
     async def mmr(self, members):
+        print(members)
         try:
+            print('try')
             temp_name = members[0]
             for member in members:
                 with DBA.DBAccess() as db:
                     check_values = db.query('SELECT mmr FROM player WHERE player_name = %s;', (member,))
         except Exception:
+            print('except')
             with DBA.DBAccess() as db:
                 check_values = db.query('SELECT mmr FROM player WHERE player_name = %s;', (members,))
         # mmrs.update('B3:B%d' % int(2+len(members)), [[member] for member in members])
