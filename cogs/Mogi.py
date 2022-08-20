dc73c74345177e69ff4c88da82c7f7b85dbf5267
billy pompey <billypompey@icloud.com>
2022-08-20 17:08:12 -0400

    a

--- cogs/Mogi.py
+++ cogs/Mogi.py
@@ -947,6 +947,7 @@ class Mogi(commands.Cog):
         #print(f'score list: {score_list}')
         player_list_check = []
         for i in range(0, len(score_list), 2):
+            print(f'{i} | {score_list[i]} | {temp[0][0]}')
             with DBA.DBAccess() as db:
                 temp = db.query('SELECT player_id FROM player WHERE player_name = %s;', (score_list[i],))
                 player_list_check.append(score_list[i])
